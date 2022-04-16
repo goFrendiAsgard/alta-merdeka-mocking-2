986fe412677ea4db246574f1b1679445d7a550ee
gofrendi <gofrendiasgard@gmail.com>
2022-04-16 14:49:22 +0700

    update instruction

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 ![](overview.png)
+
+# Run with Docker Compose
+
+```bash
+# start + create
+docker-compose up -d
+
+# start
+docker-compose start
+
+# stop
+docker-compose stop
+
+# remove
+docker-compose down
+
+# build images
+docker-compose build
+
+# push images
+docker-compose push
+```
--- main.go
+++ main.go
@@ -4,15 +4,26 @@ import (
 	"merdeka/controller"
 	"merdeka/model"
 	"merdeka/service"
+	"os"
 
 	"github.com/labstack/echo/v4"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func main() {
+	connectionString := os.Getenv("APP_DB_CONNECTION_STRING")
+	if connectionString == "" {
+		connectionString = "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	}
+
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = "8080"
+	}
+
 	e := echo.New()
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(mysql.Open(connectionString), &gorm.Config{})
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
@@ -22,5 +33,5 @@ func main() {
 	pc := controller.NewPersonController(ps)
 	e.GET("/persons", pc.Get)  // mendapatkan persons
 	e.POST("/persons", pc.Add) // menambah person
-	e.Logger.Fatal(e.Start(":8000"))
+	e.Logger.Fatal(e.Start(":" + port))
 }
